8781944e44bf4cf2091a0867b59d78efe645bb3e
yuta-hamasaki <yh02052002@gmail.com>
2024-10-04 16:29:42 -0700

    Done

---
+++ dist/ex03.js
@@ -0,0 +1,18 @@
+"use strict";
+// Create a function called combine that can accept two numbers OR two strings.
+// Use function overloading to define two versions of this function.
+// If two numbers are passed, return their sum.
+// If two strings are passed, return their concatenated (combined) value.
+function combine(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    else if (typeof a === "string" && typeof b === "string") {
+        return `${a}${b}`;
+    }
+    else {
+        throw Error("Wrong type");
+    }
+}
+console.log(combine(10, 20)); // Expected output: 30
+console.log(combine("Hello, ", "World!")); // Expected output: Hello, World!
--- src/ex05.ts
+++ src/ex05.ts
@@ -2,8 +2,12 @@
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
 
-function sumAllNumbers( ) {
-
+function sumAllNumbers(...numbers:number[]):number {
+  let answer = 0
+  for(let i = 0; i < numbers.length; i++){
+    answer = answer + numbers[i]
+  }
+  return answer
 }
 
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
